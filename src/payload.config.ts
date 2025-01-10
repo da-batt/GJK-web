b0e400c9ccf60bf9584a1dbe857a270a3b262419
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2025-01-10 13:57:31 +0100

    chore: move source code to src dir

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,9 +6,9 @@ import path from "path";
 import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
-import { Users } from "./collections/Users";
-import { Media } from "./collections/Media";
-import { Posts } from "./collections/Posts";
+import { Users } from "@/collections/Users";
+import { Media } from "@/collections/Media";
+import { Posts } from "@/collections/Posts";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
