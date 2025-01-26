8f568f85fdea187fdf736bcddf122cda8b9875d3
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2025-01-26 20:50:42 +0100

    chore: upgrade tailwindcss to v4

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ export const Hero = () => {
   return (
     <section>
       <div className="h-[36em] object-cover w-full bg-cover bg-center flex items-end p-8 bg-[url(/hero/people1.jpeg)] relative">
-        <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent to-60% opacity-75"></div>
+        <div className="absolute inset-0 bg-linear-to-t from-black to-transparent to-60% opacity-75"></div>
         <div className="z-10 text-white max-w-[50%]">
           <h1 className="display-1">Odvaha</h1>
           <p>
