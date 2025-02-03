05e0c8baceb6bf30b516083b9618b0ffe1568f5b
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2025-02-03 18:48:57 +0100

    ci: create first migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250203_174729 from './20250203_174729';
+
+export const migrations = [
+  {
+    up: migration_20250203_174729.up,
+    down: migration_20250203_174729.down,
+    name: '20250203_174729'
+  },
+];
