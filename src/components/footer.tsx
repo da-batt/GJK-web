750e62da4a8697e60e25d2038fdf7fc7f60e4511
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2025-02-11 12:34:25 +0100

    fix: footer background not spanning entire width of page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,18 +4,20 @@ import logo from "~/logo.svg";
 
 const Footer = () => {
   return (
-    <footer className="pb-12 pt-8 flex justify-between bg-black text-white container">
-      <div>
-        <Image
-          src={logo}
-          alt="Logo Gymnázia Jana Keplera"
-          className="w-52 mb-2"
-        />
-        <p>Parléřova 2, 169 00, Praha 6</p>
-        <p>+420 233 352 546</p>
-        <a href="mailto:gjk@gjk.cz">
-          <p>gjk@gjk.cz</p>
-        </a>
+    <footer className="bg-black text-white">
+      <div className="pb-12 pt-8 flex justify-between container">
+        <div>
+          <Image
+            src={logo}
+            alt="Logo Gymnázia Jana Keplera"
+            className="w-52 mb-2"
+          />
+          <p>Parléřova 2, 169 00, Praha 6</p>
+          <p>+420 233 352 546</p>
+          <a href="mailto:gjk@gjk.cz">
+            <p>gjk@gjk.cz</p>
+          </a>
+        </div>
       </div>
     </footer>
   );
