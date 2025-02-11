7e288ec114a254b9dbfa6b8d6ed229af58cde3c5
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2025-02-11 18:24:40 +0100

    fix: prevent not-found page from overlapping header

--- src/app/(app)/not-found.tsx
+++ src/app/(app)/not-found.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center flex-col min-h-[calc(100vh-3rem)] -translate-y-16">
-      <h1 className="text-[11rem] font-semibold leading-none">{"404"}</h1>
-      <p className="text-lg mb-8 text-center">Stránka nebyla nalezena</p>
-      <Button asChild>
-        <Link href="/">Zpět domů</Link>
-      </Button>
+    <div className="flex items-center justify-center flex-col min-h-[calc(100vh-3rem)]">
+      <div className="-translate-y-16 flex flex-col items-center">
+        <h1 className="text-[11rem] font-semibold leading-none">{"404"}</h1>
+        <p className="text-lg mb-8 text-center">Stránka nebyla nalezena</p>
+        <Button asChild>
+          <Link href="/">Zpět domů</Link>
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,7 +11,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
-        className="inline-flex items-center justify-center tracking-[0.05rem] bg-black rounded-full text-white px-4 py-2 gap-3 font-medium uppercase whitespace-nowrap hover:bg-neutral-100 hover:text-black transition-colors duration-200"
+        className="inline-flex w-fit items-center justify-center tracking-[0.05rem] bg-black rounded-full text-white px-4 py-2 gap-3 font-medium uppercase whitespace-nowrap hover:bg-neutral-100 hover:text-black transition-colors duration-200"
         ref={ref}
         {...props}
       />
