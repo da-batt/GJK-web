e25e4ffcc34a2bcb64d2526a2b07fd89e01c7037
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2025-02-17 16:46:12 +0100

    fix(payload): display label instead of url in dropdown link label

--- src/globals/LinksRowLabel.tsx
+++ src/globals/LinksRowLabel.tsx
@@ -7,21 +7,22 @@ import { useEffect, useState } from "react";
 const LinksRowLabel: PayloadClientReactComponent<RowLabelComponent> = () => {
   const [referenceTitle, setReferenceTitle] = useState<string | null>(null);
 
+  const { data } = useRowLabel<{
+    link?: {
+      url?: string;
+      reference?: number;
+      label?: string;
+    };
+  }>();
+
   useEffect(() => {
     if (!data.link || data.link.url) return;
     fetch(`/api/pages/${data.link.reference}?select[title]=true`)
       .then((res) => res.json())
       .then((page) => setReferenceTitle(page.title));
-  });
+  }, [data.link]);
 
-  const { data } = useRowLabel<{
-    link?: {
-      url?: string;
-      reference?: number;
-    };
-  }>();
-
-  return data.link?.url || referenceTitle || "...";
+  return data.link?.label || referenceTitle || "...";
 };
 
 export default LinksRowLabel;
