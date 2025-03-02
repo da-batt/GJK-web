c4f249c52e5d5d7004c1c3b801d406e61f050663
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2025-03-02 15:55:39 +0100

    fix(ui): show only 3 posts on homepage on large screens

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -57,7 +57,7 @@ export default async function Home() {
             </Button>
           </div>
         </FadeIn>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {posts.map((post, index) => {
             const thumbnail = post.thumbnail as Media;
             const thumbnailUrl = thumbnail.sizes?.card?.url;
