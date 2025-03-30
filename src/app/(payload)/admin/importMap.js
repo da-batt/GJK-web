ce8ccd1e03ec2912ed7746f8ee9e0a28f6357277
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2025-03-30 13:02:03 +0200

    chore: update packages

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -23,6 +23,7 @@ import { ItalicFeatureClient as ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0
 import { FixedToolbarFeatureClient as FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { default as default_1655c82726243bbac68225b66a601ba9 } from '@/globals/links-row-label.tsx'
 import { default as default_56c90203b233f40b951bfa987cf38ade } from '@/globals/tabs-row-label.tsx'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 
 export const importMap = {
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
@@ -49,5 +50,6 @@ export const importMap = {
   "@payloadcms/richtext-lexical/client#ItalicFeatureClient": ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/richtext-lexical/client#FixedToolbarFeatureClient": FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@/globals/links-row-label.tsx#default": default_1655c82726243bbac68225b66a601ba9,
-  "@/globals/tabs-row-label.tsx#default": default_56c90203b233f40b951bfa987cf38ade
+  "@/globals/tabs-row-label.tsx#default": default_56c90203b233f40b951bfa987cf38ade,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e
 }
